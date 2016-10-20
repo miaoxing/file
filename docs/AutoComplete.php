ab52dcf082f271345cb5a0ec07611028b69726b7
twinh <twinhuang@qq.com>
2016-10-20 12:19:11 +0800

    迁移plugin插件  #1828

--- docs/AutoComplete.php
+++ docs/AutoComplete.php
@@ -4,6 +4,8 @@ namespace miaoxing\file\docs {
     /**
      * @property    \Miaoxing\File\Service\File $file 文件存储服务
      * @method      \Miaoxing\File\Service\File|\Miaoxing\File\Service\File[] file()
+     *
+     * @property    \Miaoxing\File\Service\Cdn $cdn CDN服务
      */
     class AutoComplete
     {
