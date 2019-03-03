8ef87cd4b5dd0ef38e79db19df3fd5899f8ea641
twinh <twinhuang@qq.com>
2019-03-03 22:39:58 +0800

    refactoring: has-feedback => list-has-feedback

--- resources/views/files/index.php
+++ resources/views/files/index.php
@@ -3,7 +3,7 @@
 <ul class="list">
   <?php foreach ($files as $file) : ?>
     <li>
-      <a href="<?= $file['url'] ?>" class="list-item has-feedback">
+      <a href="<?= $file['url'] ?>" class="list-item list-has-feedback">
         <h4 class="list-title">
           <?= $file['originalName'] ?>
         </h4>
