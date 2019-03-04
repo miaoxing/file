8abd36f1f1643f142e0cdcfcb134fb0da4603a9a
twinh <twinhuang@qq.com>
2019-03-04 13:15:19 +0800

    refactoring: .list-feedback > .list-arrow

--- resources/views/files/index.php
+++ resources/views/files/index.php
@@ -3,11 +3,11 @@
 <ul class="list">
   <?php foreach ($files as $file) : ?>
     <li>
-      <a href="<?= $file['url'] ?>" class="list-item list-has-feedback">
+      <a href="<?= $file['url'] ?>" class="list-item list-has-arrow">
         <h4 class="list-title">
           <?= $file['originalName'] ?>
         </h4>
-        <i class="bm-angle-right list-feedback"></i>
+        <i class="bm-angle-right list-arrow"></i>
       </a>
     </li>
   <?php endforeach ?>
