f7957199147c91620f797d6be10c75f1b6861472
twinh <twinhuang@qq.com>
2021-05-07 15:36:23 +0800

    test(file): 初始化测试

---
+++ tests/pages/admin-api/files/IndexTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MiaoxingTest\File\Pages\AdminApi\Files;
+
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Plugin\Test\BaseTestCase;
+
+class IndexTest extends BaseTestCase
+{
+    public function testGet()
+    {
+        $ret = Tester::postAdminApi('files');
+
+        $this->assertRetErr($ret);
+
+        $this->assertStringContainsString('No file uploaded or the total file size is too large', $ret['message']);
+    }
+}
