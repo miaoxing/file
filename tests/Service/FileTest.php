b71c5622e07e470d19726ef640aee740985cbd8c
twinh <twinhuang@qq.com>
2021-07-09 15:42:44 +0800

    test(File): Only variables can be passed by reference

--- tests/Service/FileTest.php
+++ tests/Service/FileTest.php
@@ -16,11 +16,20 @@ class FileTest extends BaseTestCase
                 'servers' => [
                     'HTTPS' => 'on',
                     'HTTP_HOST' => 'test.com',
-                ]
-            ])
+                ],
+            ]),
         ]);
 
         $ret = $file->write('public/test.jpg');
         $this->assertSame('https://test.com/test.jpg', $ret['url']);
+    }
+
+    public function testGetFileName()
+    {
+        $file = new File();
+
+        $name = $file->getFileName('https://test.com/path/name.jpg?a=b');
+
+        $this->assertSame('name.jpg', $name);
     }
 }
