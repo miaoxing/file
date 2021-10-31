d88a1096061b20e119887dfe9db106cbd1de3cac
twinh <twinhuang@qq.com>
2021-10-31 12:24:10 +0800

    style: phpcs, php-cs-fixer

--- pages/admin-api/files/index.php
+++ pages/admin-api/files/index.php
@@ -4,14 +4,14 @@ use Miaoxing\File\Service\File;
 use Miaoxing\Plugin\BaseController;
 use Wei\Req;
 
-return new class extends BaseController {
+return new class () extends BaseController {
     public function post(Req $req)
     {
         /** @var \Wei\Upload $upload */
         $upload = wei()->upload;
         $result = $upload([
-            'name' => $req['type'] === 'image' ? '图片' : '文件',
-            'exts' => $req['type'] === 'image' ? File::getImageExts() : File::getAllExts(),
+            'name' => 'image' === $req['type'] ? '图片' : '文件',
+            'exts' => 'image' === $req['type'] ? File::getImageExts() : File::getAllExts(),
             'dir' => File::getUploadDir(),
             'fileName' => File::getUploadName(),
         ]);
