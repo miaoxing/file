446cfc05ed21486db4602c1e301bffd5cfd141e2
twinh <twinhuang@qq.com>
2022-02-14 21:09:17 +0800

    test(file): 更新测试

--- tests/Service/FileTest.php
+++ tests/Service/FileTest.php
@@ -4,32 +4,14 @@ namespace MiaoxingTest\File\Service;
 
 use Miaoxing\File\Service\File;
 use Miaoxing\Plugin\Test\BaseTestCase;
-use Wei\Req;
 
 class FileTest extends BaseTestCase
 {
-    public function testWriteRetContainsFullUrl()
+    public function testSaveLocal()
     {
-        $file = new File([
-            'req' => new Req([
-                'fromGlobal' => false,
-                'servers' => [
-                    'HTTPS' => 'on',
-                    'HTTP_HOST' => 'test.com',
-                ],
-            ]),
-        ]);
+        $ret = File::saveLocal(__FILE__);
 
-        $ret = $file->write('public/test.jpg');
-        $this->assertSame('https://test.com/test.jpg', $ret['url']);
-    }
-
-    public function testGetFileName()
-    {
-        $file = new File();
-
-        $name = $file->getFileName('https://test.com/path/name.jpg?a=b');
-
-        $this->assertSame('name.jpg', $name);
+        $this->assertRetSuc($ret);
+        $this->assertNotEmpty($ret['data']['url']);
     }
 }
