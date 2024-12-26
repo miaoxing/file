c25b950aa0a182c263edeb1b2f26e2e31605e3ac
twinh <twinhuang@qq.com>
2024-12-26 22:12:04 +0800

    feat(file): 后台管理员都允许上传文件

--- pages/api/admin/files/index.php
+++ pages/api/admin/files/index.php
@@ -1,11 +1,18 @@
 <?php
 
+use Miaoxing\App\Middleware\CheckPagePermission;
 use Miaoxing\File\Service\File;
 use Miaoxing\Plugin\BasePage;
 use Miaoxing\Plugin\Service\Upload;
 use Wei\Req;
 
 return new class extends BasePage {
+    public function init()
+    {
+        parent::init();
+        $this->removeMiddleware(CheckPagePermission::class);
+    }
+
     public function post(Req $req)
     {
         $ret = Upload::save([
