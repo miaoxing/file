7631f17051aadc302cc81b373a56230d4f0ce172
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/FileModel.php
+++ src/Service/FileModel.php
@@ -2,15 +2,31 @@
 
 namespace Miaoxing\File\Service;
 
-use Miaoxing\File\Metadata\FileTrait;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\HasAppIdTrait;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
 
+/**
+ * @property string|null $id
+ * @property string $appId
+ * @property string $origName
+ * @property string $path
+ * @property int $type 文件类型,1是图片,2是文档,3是音频,4是视频
+ * @property string $ext 扩展名
+ * @property int $size
+ * @property int $width
+ * @property int $height
+ * @property string $md5
+ * @property string $url
+ * @property string|null $origCreatedAt
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ */
 class FileModel extends BaseModel
 {
-    use FileTrait;
     use HasAppIdTrait;
     use ModelTrait;
     use SnowflakeTrait;
